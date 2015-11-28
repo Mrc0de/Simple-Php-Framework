70e4c122ab3b9790fefa024efe172282c1c79a23
N <neurosys@neuroticnetworks.com>
2015-11-28 14:18:30 -0800

    Started on index.php

---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<?php
+    //At this point, we draw(echo) nothing
+    //First we must load the Page Object
+    //Then we configure it
+    //Since this is index.php, its always a landing style page
+    //For this example, the landing page is a register or login page
+    //Base is generic and empty, user can modify to generate new pages
+
+
+
+?>
