4e7ce0e8ad6569ac5766e86a9bf20862fb99ee77
N <neurosys@neuroticnetworks.com>
2015-12-02 20:11:31 -0800

    Started the page creation process

--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,25 @@
     //For this example, the landing page is a register or login page
     //Base is generic and empty, user can modify CLASS (or subclass it) to generate new pages
     //Stylesheets, Javascripts and even php scripts can be plugged via filename (mind that working directory)
+
+    //Server gets mad if we dont set timezone now
+    date_default_timezone_set('America/Los_Angeles');
+
+    //Require the classes/genPageClass.php.inc	(remember we are not actually in classes directory)
+    require_once('classes/genPageClass.php.inc');
     
-
-
+    //Create thisPage
+    $thisPage = new genPageClass();
+    
+    //Setup the page (Change these freely, each page or use config file) (TODO: make config file stuff)
+    $thisPage->config['baseCSS'] = 'genPageBase.css';
+    $thisPage->config['menuCSS'] = 'navMenu.css';
+    $thisPage->config['rootURL'] = 'http://www.geekprojex.com/';
+    $thisPage->config['pageTitle'] = 'GeekProjex.com';
+    //Database config is stored in inc/dbconf.php.inc
+    //Edit it once, all use the same file. (via runQuery)
+    
+    //draw the page
+    $thisPage->draw();
+    //Yea... thats all. The rest is in the class.
 ?>
